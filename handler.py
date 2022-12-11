7a322ba29383857e5b44356ce0ef6e8ae705d09a
Mário Brum <97545607+brummario@users.noreply.github.com>
2022-12-11 00:32:45 -0300

    Update handler.py
    fix os module

--- handler.py
+++ handler.py
@@ -43,5 +43,5 @@ def rossmann_predict():
         print( test_json[0].keys() )# Response( '{}', status=200, mimetype='application/json' )
 
 if __name__ == '__main__':
-    port = os.envarion.get( 'PORT', 5000 )
+    port = os.environ.get( 'PORT', 5000 )
     app.run( host='0.0.0.0', port=port )
